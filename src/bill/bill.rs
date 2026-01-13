4914a90a9b49d8a3e1de1bfa562817dfaf477201
Thomas Prosser <tp@thomit.com>
2026-01-13 22:53:40 +0100

    bill

---
+++ src/bill/bill.rs
@@ -0,0 +1,16 @@
+/// This is all in Millimeters and all in DIN A4
+
+//! A4 Width in mm
+const A4_PORTRAIT_WIDTH: u8 = 210;
+
+//! A4 Height in mm
+const A4_PORTRAIT_HEIGHT: u8 = 297;
+
+//! Width of a QR Slip is incidentally the same as the whole paper slip 🤣
+const QR_BILL_SLIP_WIDTH: u8 = 210;
+
+//! Now the height of the slip itself
+const QR_BILL_HEIGHT: u8 = 105;
+
+//! Then the width with scissors symbol and all... Surprise
+const QR_BILL_WITH_ALL_WIDTH = 210;
