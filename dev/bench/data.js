7d7e58e4f109c540640b40b31f0511aba3b3cf49
github-action-benchmark <github@users.noreply.github.com>
2026-07-25 10:08:53 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for c8304da82adc23d94f37f41c918a195013743e4c

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1784974133191,
+  "repoUrl": "https://github.com/thomi137/swissqrust",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Thomas Prosser",
+            "email": "tp@thomit.com"
+          },
+          "committer": {
+            "name": "Thomas Prosser",
+            "email": "tp@thomit.com"
+          },
+          "id": "c8304da82adc23d94f37f41c918a195013743e4c",
+          "message": "added benchmark tests",
+          "timestamp": "2026-07-25T10:00:35Z",
+          "url": "https://github.com/thomi137/swissqrust/commit/c8304da82adc23d94f37f41c918a195013743e4c"
+        },
+        "date": 1784974131235,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "render_bill_to_pdf",
+            "value": 9638515.303333336,
+            "unit": "ns"
+          },
+          {
+            "name": "render_bill_to_svg",
+            "value": 4818298.637272727,
+            "unit": "ns"
+          }
+        ]
+      }
+    ]
+  }
+}
